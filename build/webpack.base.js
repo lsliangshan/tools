bbc7168c89ed5022c4aafb937014207422423cad
梁山 <liangshan@192.168.0.107>
2017-06-21 22:52:50 +0800

    Webpack font-awesome

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -54,10 +54,18 @@ module.exports = {
         }
       },
       {
-        test: /\.(ttf|eot|woff|woff2)$/,
-        loader: 'file-loader?publicPath=./&outputPath=fonts/'
-        // loader: 'file-loader?name=fonts/[name].[ext]'
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
       },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader'
+      },
+      // {
+      //   test: /\.(ttf|eot|woff|woff2)$/,
+      //   loader: 'file-loader?publicPath=./&outputPath=fonts/'
+      //   // loader: 'file-loader?name=fonts/[name].[ext]'
+      // },
       {
         test: /\.svg$/,
         loader: 'raw-loader'
